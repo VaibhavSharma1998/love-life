02c1d2f685f76ad3e7707159e24aea4ca5225fcd
VaibhavSharma1998 <83278317+VaibhavSharma1998@users.noreply.github.com>
2023-03-17 17:57:39 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Vaibhav Sharma's Love life project on 17th march 2023</h1>
+    </div>
+  )
+}
+
+export default Navbar
